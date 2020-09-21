8acd9c5c425723b82444f31fdbd0c4698b9f4456
hatem1055 <hatemmostafa31@gmail.com>
2020-09-21 23:50:45 +0200

    homepage

---
+++ src/pages/homepage/hompage.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../components/dirctory/drictory.components'
+
+const HomPage = () =>(
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default HomPage
